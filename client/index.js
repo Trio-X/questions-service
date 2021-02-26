b5abef717dac5c73908ce6d2bec8b83121f161ec
97Fakhreddine <fakhri19971998@gmail.com>
2021-02-26 23:03:23 +0100

    Searchbar refactored successfully && redux store created

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./components/App.jsx";
-
+import store from "./redux-store/store.js";
 ReactDOM.render(<App />, document.getElementById("app"));
