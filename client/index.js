511206a75e46f340e043978a0067a281a2d7eb73
97Fakhreddine <fakhri19971998@gmail.com>
2021-02-27 16:43:07 +0100

    the question has been renedered with hooks state

--- client/index.js
+++ client/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./components/App.jsx";
 import store from "./redux-store/store.js";
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
