bf001466ba314ff380fc649596a89672d947ee07
97Fakhreddine <fakhri19971998@gmail.com>
2021-03-01 11:17:35 +0100

    created and renedered the answers data per question

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT;
 const { routerQuestionList } = require("./routers/routerQuestionsList");
+const { routerAnswersList } = require("./routers/routerAnswersList");
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
-
 /**
  * applying @routerQuestionList as a middleware
  *
  * */
 app.use("/q", routerQuestionList);
+app.use("/a", routerAnswersList);
 // the function below is just an example...
 /**
  * @function addition
